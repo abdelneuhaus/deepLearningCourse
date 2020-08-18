3ca19a510a34acbe6679daa4643db00c3252d80c
abdelneuhaus <abdelghani.neuhaus@etu.u-bordeaux.fr>
2020-08-18 19:04:48 +0200

    Adding the next exercise

---
+++ planer_data_classification_with_one_hidden_layer/script.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
